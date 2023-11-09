f65f29ae8056ea96dabc47bf896bed3323b6c3e7
Juan Almanza <hi@scidroid.co>
2023-11-08 22:48:25 -0500

    fix cards background color

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,15 +3,17 @@ import { ReactNode } from "react";
 export function Card({
   children,
   hover,
-  className
+  className,
+  bg = "white"
 }: {
   children: ReactNode;
   hover?: boolean;
   className?: string;
+  bg?: string;
 }) {
   return (
     <div
-      className={`w-full bg-white rounded-md border-2 px-8 py-6 ${
+      className={`w-full bg-${bg} rounded-md border-2 px-8 py-6 ${
         hover ? "shadow-inner hover:shadow-md transition-shadow" : ""
       } ${className}`}
     >
